d10a6d23df903829455bcdf3e6f83c6cbf42f5b2
Andrian-Maulana999 <145946116+Andrian-Maulana999@users.noreply.github.com>
2023-12-04 10:40:52 +0800

    TUGAS ALGORITMA PERTAMA

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //membuat variabel dan tipe datanya
+        String nama;
+        String nim;
+        String prodi;
+
+        //mengisi variabel
+        nama = "Andrian Maulana";
+        nim = "23241020";
+        prodi = "PTI";
+
+        //menampilkan isi variabel ke layar monitor
+        System.out.println("Nama Mahasiswa : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Program Studi :  " + prodi);
+    }
+}
+ 
---
+++ 02-InputOuput/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //deklarasi variabel dan tipe data
+        String nama;
+        int nim;
+        String prodi;
+        Double tinggi_badan;
+
+        //membuat scanner baru
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Form Data Mahasiswa UNDIKMA");
+        System.out.println("===========================");
+        //membuat form
+        System.out.print("Masukan Nama Mahasiswa : ");
+        nama = input.nextLine();
+
+        System.out.print("Masukan Prodi : ");
+        prodi = input.nextLine();
+
+        System.out.print("Masukan NIM : ");
+        nim = input.nextInt();
+
+        System.out.print("Tinggi Badan : ");
+        tinggi_badan = input.nextDouble();
+
+        //cetak output ke layar monitor
+        System.out.println("Data Mahasiswa UNDIKMA");
+
+
+
+    }
+}
